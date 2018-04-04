4c431db0e0b8aaa4782e475d90e85d3510ee716b
Julietta Hensgen <juliettaurdiales@yahoo.com>
2018-04-04 10:45:21 -0500

    Adding files

---
+++ bash/string.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+STRING1='Hello'
+STRING2='World'
+echo "${STRING1} ${STRING2}"
+
+STRING1='Mommy'
+STRING2='Dearest'
+echo "${STRING1} ${STRING2}"
+
+
+STRING1='Coding'
+STRING2='Paul'
+echo "${STRING1} ${STRING2}"
